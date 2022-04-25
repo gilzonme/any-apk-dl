5f2f65deef8f749d53d2c9dc2c3edc7e6e702b56
gilson <me.gilsongilbert@gmail.com>
2022-04-25 18:16:29 +0530

    Updated apk-dl

--- package/index.js
+++ package/index.js
@@ -1,15 +1,10 @@
 const providerConfig = require("./config/provider.json")
-const defaultProvider = "apk-dl.com"
 function getRandomProvider(obj) {
-    const keys = Object.keys(obj);
-  
-    return keys[Math.floor(Math.random() * keys.length)];
-  }
-function apkDl() {
-    provider=getRandomProvider(providerConfig);
-    const providerObject = require("./providers/" + providerConfig[provider])
-    return providerObject
-
+  const keys = Object.keys(obj);
+  return keys[Math.floor(Math.random() * keys.length)];
 }
-
+function apkDl() {
+  const downloaderObject = require("./config/download")
+  return downloaderObject
+}
 module.exports = apkDl
